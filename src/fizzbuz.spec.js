2b44094492e79a0b1560e6383d03e2116382dfac
Israel Antezana Rojas <israelantezana@gmail.com>
2024-12-05 11:24:08 -0400

    Primer commit

---
+++ src/fizzbuz.spec.js
@@ -0,0 +1,11 @@
+//import sumar from "./sumador.js";
+
+describe("Fizbuzz", () => {
+  it("deberia retornar el mismo numero si no sigue ningun regla", () => {
+    expect(generarFizzBuzz(1)).toEqual("1");
+  });
+});
+
+function generarFizzBuzz(n) {
+  return "1";
+}
