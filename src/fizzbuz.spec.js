ad0dc3aa796e06444d01091372b0aa19d93a1fa1
Israel Antezana Rojas <israelantezana@gmail.com>
2024-12-05 11:25:48 -0400

    Toma en cuenta el caso del 3

--- src/fizzbuz.spec.js
+++ src/fizzbuz.spec.js
@@ -4,8 +4,15 @@ describe("Fizbuzz", () => {
   it("deberia retornar el mismo numero si no sigue ningun regla", () => {
     expect(generarFizzBuzz(1)).toEqual("1");
   });
+
+  it("deberia retornar fizz si sigue la regla de 3", () => {
+    expect(generarFizzBuzz(3)).toEqual("Fizz");
+  });
 });
 
 function generarFizzBuzz(n) {
+  if (n == 3) {
+    return "Fizz";
+  }
   return "1";
 }
